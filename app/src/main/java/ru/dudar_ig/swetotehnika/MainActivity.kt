303adf52e54156648806328f95190a17c1da251b
DudarIg <dudar.ig@yandex.ru>
2022-01-03 16:04:47 +0300

    res

--- app/src/main/java/ru/dudar_ig/swetotehnika/MainActivity.kt
+++ app/src/main/java/ru/dudar_ig/swetotehnika/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.dudar_ig.swetotehnika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.dudar_ig.swetotehnika.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
